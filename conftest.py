88f3403f7623774ef46fc1c48ec4e71ad590561d
TennoGODD <kustov.mixail228@gmail.com>
2025-10-28 01:18:37 +0300

    Third try

--- conftest.py
+++ conftest.py
@@ -6,9 +6,14 @@ from selenium.webdriver.chrome.options import Options
 def driver(request):
     options = Options()
     options.add_argument('--headless')
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920x1080")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--remote-debugging-port=9222')
+    options.add_argument('--user-data-dir=/tmp/chrome_profile')
+
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     request.cls.driver = driver
